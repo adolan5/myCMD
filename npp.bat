b6ceae6182dcc3bf881e1a9eb7646cf788449200
adolan5 <then3rd3.14@gmail.com>
2017-08-31 13:40:05 -0600

    Make npp.bat more modular with exe location

--- npp.bat
+++ npp.bat
@@ -3,8 +3,12 @@ rem -- Run notepad++ --
 rem Much better alternative to adding it to your PATH, don't you think?
 rem Inspired in part by the scripts used for gvim
 
-rem The location of your notepad++ installation/executable
-set NPP_EXE_DIR=C:\Program Files (x86)\Notepad++
+rem Check if you've set your executable location
+if "%NPP_EXE_DIR%"== "" (
+	echo "NPP_EXE_DIR not set! Do you need to add it to your startup.bat?"
+	goto eof
+)
+
 if exist "%NPP_EXE_DIR%\notepad++.exe" goto havenpp
 echo "%NPP_EXE_DIR%\notepad++.exe not found!"
 goto eof
