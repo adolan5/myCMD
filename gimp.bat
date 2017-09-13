25887a94b20ffb54a4c2b87bb394832ce705e2d8
adolan5 <then3rd3.14@gmail.com>
2017-09-12 21:50:46 -0600

    Implement script to launch gimp 2.8 from mycmd

---
+++ gimp.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal EnableDelayedExpansion
+rem -- Run gimp 2.8 --
+rem Check if you've set your executable location in your startup.bat
+if "%GIMP_EXE_DIR%"== "" (
+	echo GIMP_EXE_DIR not set^^! Do you need to add it to your startup.bat?
+	goto eof
+)
+
+if exist "%GIMP_EXE_DIR%\gimp-2.8.exe" goto havegimp
+echo "%GIMP_EXE_DIR%\gimp-2.8.exe not found!"
+goto eof
+
+:havegimp
+set GIMPARGS=
+:loopstart
+if .%1==. goto loopend
+rem This line will resolve the full path for each file passed in; including for use of wildcards
+rem Needed because gimp-2.8.exe does not accomodate wildcards
+for %%a in (%1) do set GIMPARGS=!GIMPARGS! "%%~dpnxa"
+shift
+goto loopstart
+
+:loopend
+start "" "%GIMP_EXE_DIR%\gimp-2.8.exe" %GIMPARGS%
+goto eof
+
+:eof
+set GIMPARGS=
