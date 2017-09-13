90512084b194a31a3b21caa7cc68147f9dddf278
adolan5 <then3rd3.14@gmail.com>
2017-09-12 21:53:33 -0600

    Updated startup example bat

--- startup_example.bat
+++ startup_example.bat
@@ -10,6 +10,8 @@ doskey home=cd "%USERPROFILE%"
 
 rem The location of your notepad++ installation/executable
 set NPP_EXE_DIR=C:\Program Files (x86)\Notepad++
+rem The location of your gimp 2.8 installation/executable
+set GIMP_EXE_DIR=C:\Program Files\GIMP 2\bin
 
 rem Example of an 'alias'
 rem doskey p=path\to\putty.exe
